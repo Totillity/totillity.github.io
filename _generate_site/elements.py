b0cd4b29fd9bc6a57b8fd513bd3b6acdc1790130
Totillity <39473801+Totillity@users.noreply.github.com>
2020-11-13 09:41:15 -0700

    Create Quote Blocks, Headings, and Page endings

--- _generate_site/elements.py
+++ _generate_site/elements.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-import textwrap
+from textwrap import dedent
 from pathlib import Path
 from typing import List
 
@@ -19,21 +19,6 @@ def inline_code(s: str) -> str:
     return f"<code>{s}</code>"
 
 
-class Page:
-    def __init__(self, path: str, nav_loc: List[str]):
-        self.path = Path(path)
-        self.nav_loc = nav_loc
-
-        self.elements: List[Element] = []
-
-    def __iadd__(self, other: Element):
-        self.elements.append(other)
-        return self
-
-    def render(self):
-        return "".join(element.render() for element in self.elements)
-
-
 class Element:
     type: str
 
@@ -44,7 +29,7 @@ class Element:
 class Text(Element):
     def __init__(self, text: str):
         self.type = "Text"
-        self.text = textwrap.dedent(text)
+        self.text = dedent(text)
 
     def render(self) -> str:
         return f'{self.text}'
@@ -53,7 +38,7 @@ class Text(Element):
 class Paragraph(Element):
     def __init__(self, text: str):
         self.type = "Paragraph"
-        self.text = textwrap.dedent(text)
+        self.text = dedent(text)
 
     def render(self) -> str:
         return f'<div class="content-paragraph"><p>{self.text}</p></div>'
@@ -68,10 +53,28 @@ class Title(Element):
         return f'<div class="content-title"><header>{self.title_text}</header></div><hr>'
 
 
+class Heading(Element):
+    def __init__(self, heading_text: str):
+        self.type = "Heading"
+        self.heading_text = heading_text
+
+    def render(self) -> str:
+        return f'<div class="content-heading">{self.heading_text}</div>'
+
+
+class Quote(Element):
+    def __init__(self, text: str, caption: str):
+        self.text = dedent(text)
+        self.caption = dedent(caption)
+
+    def render(self) -> str:
+        return f'<figure class="content-quote"><blockquote>{self.text}</blockquote><figcaption>&mdash; {self.caption}</figcaption></figure>'
+
+
 class Code(Element):
     def __init__(self, code: str):
         self.type = "Code"
-        self.code = textwrap.dedent(code)
+        self.code = dedent(code)
 
     def render(self) -> str:
         return f'<div class="content-code"><pre>{self.code}</pre></div>'
@@ -85,3 +88,26 @@ class OrderedList(Element):
     def render(self) -> str:
         items = "".join(f'<li class="content-ordered-list-item">{item.render()}</li>' for item in self.items)
         return f'<ol class="content-ordered-list">{items}</ol>'
+
+
+class Page:
+    class End(Element):
+        def __init__(self):
+            pass
+
+        def render(self) -> str:
+            return f'<div class="content-end"></div>'  # (c) Magilan Sendhil 2020
+
+    def __init__(self, path: str, nav_loc: List[str]):
+        self.path = Path(path)
+        self.nav_loc = nav_loc
+
+        self.elements: List[Element] = []
+
+    def __iadd__(self, other: Element):
+        self.elements.append(other)
+        return self
+
+    def render(self):
+        elements = self.elements + [self.End()]
+        return "".join(element.render() for element in elements)
