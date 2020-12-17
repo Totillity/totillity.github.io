63772d5cd11ad44b94d2ccdfe730b094cc69452e
Totillity <39473801+Totillity@users.noreply.github.com>
2020-12-17 10:43:23 -0700

    Added week 5

--- _site/jit-project/progress_timeline/week_5.py
+++ _site/jit-project/progress_timeline/week_5.py
@@ -8,14 +8,22 @@ page += Paragraph("November 14, 2020 - November 20, 2020")
 
 page += Heading("Goals For This Week")
 page += OrderedList([
+    Paragraph('Look through current languages and see what I want mine to look like'),
+    Paragraph('Based on my previous work, I can see what language design would lend itself best to JITing'),
+    Paragraph('Design such a language'),
 ])
 
-page += Heading("Journal For This Week")
-page += Paragraph(f"""
+page += Heading("Thoughts")
+page += Paragraphs(f"""
+Looking at previous languages I used, and the work that I've don already, I think that my language needs a lot of 
+flexibility. I want my language to be able to target multiple other languages (such as Python, Javascript, and Lua).
+At the very least, I'll need very flexible hash-maps (dictionaries in python and objects in javascript) which can 
+have multiple behaviors. After all, each language I listed above treats them differently. 
+
+Beyond that, my default language will probably pull from python-style significant indentation. It looks much nicer,
+especially because it wastes less space on '{{' and '}}'. I will need to see how python parses, it, though, because I don't
+know for the life of me.
 """)
 
-page += Paragraph("Sources:")
-page += OrderedList([
-])
 
 __pages__ = [page]
